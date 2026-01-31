66b66ff6c5b6e0aae0d95dc9236dad3314803225
toucheres <761844639@qq.com>
2026-01-31 19:00:53 +0800

    add test

--- test/test1.c
+++ test/test1.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    return printf("%d%c%d\n", 1, '2', 3);
+    FILE* fp = fopen("/home/toucher/vscoderope/mycomplier/test/testfile.txt", "r+");
+    char arr[33];
+    fread(arr, 1, 100, fp);
+    printf(arr);
+    char files[30] = "ciallo world\n";
+    fwrite(files, 1, strlen(files), fp);
+    return 0;
 }
