231f19b2759dc5314e3e8833e936d030473a84ff
toucheres <761844639@qq.com>
2026-02-04 18:04:17 +0800

    enable format for vsprinf

--- stdlibc/stdio.c
+++ stdlibc/stdio.c
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <systemcall.h>
+#include <string.h>
 void write(long arg)
 {
     return __write(arg);
@@ -47,7 +48,9 @@ long num_to_str(char* src, long num)
         return len + 1;
     }
 }
-// 仅支持%s %d %c %ld %p %%
+
+// 支持 %[width][.precision]s %[width]d %c %ld %p %%
+// 例如: %8.4s (宽度8，精度4) %8d (宽度8)
 long vsprintf(char* dst, char* fmt, va_list ap)
 {
     char* src = dst;
@@ -58,31 +61,123 @@ long vsprintf(char* dst, char* fmt, va_list ap)
         if (*fmt == '%')
         {
             fmt = fmt + 1;
+
+            // 解析标志：左对齐 '-'
+            int left_align = 0;
+            if (*fmt == '-')
+            {
+                left_align = 1;
+                fmt = fmt + 1;
+            }
+
+            // 解析宽度
+            int width = 0;
+            while (*fmt >= '0' && *fmt <= '9')
+            {
+                width = width * 10 + (*fmt - '0');
+                fmt = fmt + 1;
+            }
+
+            // 解析精度
+            int precision = -1; // -1 表示未指定
+            if (*fmt == '.')
+            {
+                fmt = fmt + 1;
+                precision = 0;
+                while (*fmt >= '0' && *fmt <= '9')
+                {
+                    precision = precision * 10 + (*fmt - '0');
+                    fmt = fmt + 1;
+                }
+            }
+
             if (*fmt == 'c')
             {
                 char ch = va_arg(ap, int); // char 提升为 int
+                // 宽度填充
+                long pad = 0;
+                if (width > 1) { pad = width - 1; }
+                if (!left_align)
+                {
+                    long p = pad;
+                    while (p > 0) { *src++ = ' '; charsnum++; p--; }
+                }
                 *src = ch;
                 charsnum = charsnum + 1;
-                fmt = fmt + 1;
                 src = src + 1;
+                if (left_align)
+                {
+                    long p = pad;
+                    while (p > 0) { *src++ = ' '; charsnum++; p--; }
+                }
+                fmt = fmt + 1;
             }
             else if (*fmt == 'd')
             {
                 int val = va_arg(ap, int);
-                long size = num_to_str(src, val);
-                charsnum = charsnum + size;
+                char tmpbuf[32];
+                long size = num_to_str(tmpbuf, val);
+                tmpbuf[size] = '\0';
+
+                long pad = 0;
+                if (width > size) { pad = width - size; }
+                if (!left_align)
+                {
+                    long p = pad;
+                    while (p > 0) { *src++ = ' '; charsnum++; p--; }
+                }
+                for (long i = 0; i < size; i++)
+                {
+                    *src++ = tmpbuf[i];
+                    charsnum++;
+                }
+                if (left_align)
+                {
+                    long p = pad;
+                    while (p > 0) { *src++ = ' '; charsnum++; p--; }
+                }
                 fmt = fmt + 1;
-                src = src + size;
             }
             else if (*fmt == 's')
             {
-                char* str = va_arg(ap, char*);   
-                while (*str != '\0')
+                char* str = va_arg(ap, char*);
+                
+                // 内联计算字符串长度，不依赖 strlen
+                // long slen = 0;
+                // char* tmp = str;
+                // while (*tmp != '\0') { slen++; tmp++; }
+                long slen = strlen(str);
+
+                // precision 限制输出字符数
+                long outlen = slen;
+                if (precision >= 0 && precision < outlen)
                 {
-                    *src = *str;
-                    src = src + 1;
-                    str = str + 1;
-                    charsnum = charsnum + 1;
+                    outlen = precision;
+                }
+
+                // 计算填充数
+                long pad = 0;
+                if (width > outlen)
+                {
+                    pad = width - outlen;
+                }
+                // 右对齐时先填充空格
+                if (!left_align)
+                {
+                    long p = pad;
+                    while (p > 0) { *src++ = ' '; charsnum++; p--; }
+                }
+                // 输出字符串内容
+                for (long i = 0; i < outlen; i++)
+                {
+                    *src++ = str[i];
+                    charsnum++;
+                }
+                // 左对齐时后填充空格
+                if (left_align)
+                {
+                    long p = pad;
+                    while (p > 0) { *src++ = ' '; charsnum++; p--; }
                 }
                 fmt = fmt + 1;
             }
@@ -92,10 +187,28 @@ long vsprintf(char* dst, char* fmt, va_list ap)
                 if (*fmt == 'd')
                 {
                     long val = va_arg(ap, long);
-                    long size = num_to_str(src, val);
-                    charsnum = charsnum + size;
+                    char tmpbuf[32];
+                    long size = num_to_str(tmpbuf, val);
+                    tmpbuf[size] = '\0';
+
+                    long pad = 0;
+                    if (width > size) { pad = width - size; }
+                    if (!left_align)
+                    {
+                        long p = pad;
+                        while (p > 0) { *src++ = ' '; charsnum++; p--; }
+                    }
+                    for (long i = 0; i < size; i++)
+                    {
+                        *src++ = tmpbuf[i];
+                        charsnum++;
+                    }
+                    if (left_align)
+                    {
+                        long p = pad;
+                        while (p > 0) { *src++ = ' '; charsnum++; p--; }
+                    }
                     fmt = fmt + 1;
-                    src = src + size;
                 }
                 else
                 {
